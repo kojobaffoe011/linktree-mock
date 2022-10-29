90cc2e04c58a807fe8dbf7dfb34a2d4df02652a9
Kojo Baffoe <kojobaffoe935@gmail.com>
2022-10-28 23:10:50 -0400

    fix: Links.js

--- src/components/Links.js
+++ src/components/Links.js
@@ -24,17 +24,21 @@ const Links = () => {
   return (
     <div className=" links-padding">
       <Flex stack={true} spacing={24}>
-        <a href="https://google.com" target="_blank">
+        <a
+          href="https://twitter.com/kojobaffoe100"
+          target="_blank"
+          title="follow this dev on twitter!"
+        >
           <Link text={"Twitter Link"} />
         </a>
         <a href="https://training.zuri.team/" target="_blank" id="btn__zuri">
           <Link text={"Zuri Team"} />
         </a>
         <a
-          title="find books about design and coding here!"
           href="https://books.zuri.team"
           target="_blank"
           id="books"
+          title="find books about design and coding here!"
         >
           <Link text={"Zuri Books"} />
         </a>
@@ -42,16 +46,23 @@ const Links = () => {
           href="https://books.zuri.team/python-for-beginners?ref_id=buff"
           target="_blank"
           id="book__python"
+          title="purchase books and learn all there is to know in your chosen field!"
         >
           <Link text={"Python Books"} />
         </a>
-        <a href="https://background.zuri.team" target="_blank" id="pitch">
+        <a
+          href="https://background.zuri.team"
+          target="_blank"
+          id="pitch"
+          title="do background checks for coders here :)"
+        >
           <Link text={"Background Check for Coders"} />
         </a>
         <a
           href="https://books.zuri.team/design-rules"
           target="_blank"
           id="book__design"
+          title="patronize free design books offered by Zuri!"
         >
           <Link text={"Design Books"} />
         </a>
