2f48e2791372463da9a317ef79f0c47295dcce9a
Kojo Baffoe <kojobaffoe935@gmail.com>
2022-11-05 01:38:33 -0500

    style: ContactPage.js

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -22,7 +22,7 @@ const ContactPage = () => {
                   <label>
                     <p className="gray-700 smallest">First Name</p>
                   </label>
-                  <input placeholder="Enter your first name" />
+                  <input placeholder="Enter your first name" id="first_name" />
                 </Flex>
               </GridItem>
               <GridItem span={6} md={12}>
@@ -30,7 +30,7 @@ const ContactPage = () => {
                   <label>
                     <p className="gray-700 smallest">Last Name</p>
                   </label>
-                  <input placeholder="Enter your last name" />
+                  <input placeholder="Enter your last name" id="last_name" />
                 </Flex>
               </GridItem>
             </Grid>
@@ -38,7 +38,7 @@ const ContactPage = () => {
               <label>
                 <p className="gray-700 smallest">Email</p>
               </label>
-              <input placeholder="yourname@email.com" />
+              <input placeholder="yourname@email.com" id="email" />
             </Flex>
             <Flex stack={true} spacing={6}>
               <label>
@@ -47,24 +47,31 @@ const ContactPage = () => {
               <textarea
                 style={{ height: "132px" }}
                 placeholder="Send me a message and I'll reply you as soon as possible..."
+                id="message"
               />
             </Flex>
-            <Flex jc="flex-start" className="">
-              <div>
+            <Flex stack={true} spacing={32}>
+              <Flex className="" spacing={12}>
                 <input
+                  className=""
                   type="checkbox"
                   style={{
                     margin: 0,
                     height: "20px",
                     width: "20px",
                     borderRadius: "6px",
+                    border: "1px solid #d0d5dd",
                   }}
                 />
-              </div>
+                <p className="gray-600 ">
+                  You agree to providing your data Kojo Baffoe to who may
+                  contact you.
+                </p>
+              </Flex>
+              <button>
+                <p className="white bold smaller">Send Message</p>
+              </button>
             </Flex>
-            <button>
-              <p className="white bold">Send Message</p>
-            </button>
           </Flex>
         </form>
       </Flex>
